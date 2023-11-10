c56d65a839d88c0d5a2811eb3edc8a0a4f341d02
AndryAvamagi <avamagi.andry@gmail.com>
2023-11-10 11:03:44 +0200

    korrastasin failistruktuuri

---
+++ scripts/script_opacity.js
@@ -0,0 +1,9 @@
+const background = document.querySelector('.background-image'); // valib klassi ".background-image"
+
+window.addEventListener('scroll', () => { //iga scrolliga, tee seda funktsiooni
+  const scrollPos = window.scrollY; //aknas hiire y koordinaat
+  const opacity = 1 - scrollPos / 1000; // arvuta opacity
+  background.style.opacity = opacity.toFixed(2); // muuda klassi opacityt
+});
+
+  
